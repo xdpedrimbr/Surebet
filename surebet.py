86783f95da96e0466b99538dea8587cb8825cdff
Pedro Henrique Goncalves Teixeira <pedrohgt73@gmail.com>
2023-03-31 14:51:19 -0300

    tentativa de retornar a div

--- surebet.py
+++ surebet.py
@@ -1,4 +1,5 @@
 import bs4
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -6,21 +7,26 @@ import time
 driver = webdriver.Chrome()
 driver.get('https://www.esportenetvip.com.br/')
 
-content = driver.page_source
-soup = bs4.BeautifulSoup(content, "html.parser")
+html = driver.page_source
+soup = bs4.BeautifulSoup(html, "html.parser")
+items = soup.find("div", {"id": "grdJogos"})
+
+
+if items:
+    print(items.text)
+else:
+    print('Div não encontrada')
 
 time.sleep(4)
 
 div = driver.find_element(By.ID, "grdJogos")
 linhas = div.find_elements(By.XPATH, ".//tr")
 
-print(linhas)
-
-for linha in linhas:
-    colunas = linha.find_elements(By.XPATH, ".//td")
-    for coluna in colunas:
-        informacao = coluna.text
-        print(informacao)
+# for linha in linhas:
+#     colunas = linha.find_elements(By.XPATH, ".//td")
+#     for coluna in colunas:
+#         informacao = coluna.text
+#         print(informacao)
 
 
 driver.quit()
